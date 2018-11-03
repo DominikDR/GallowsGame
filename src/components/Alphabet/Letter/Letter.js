8a4377b938634486509da4568047e35625766157
Dominik <dominik@pl.pl>
2018-11-03 18:21:05 +0100

    Created UI and styles for clicked letter. Enlarging the space between words. Fixed result field

--- src/components/Alphabet/Letter/Letter.js
+++ src/components/Alphabet/Letter/Letter.js
@@ -1,34 +1,69 @@
 import React from 'react';
 import styles from './Letter.css';
+import classnames from 'classnames';
 
-const checkLetter = (letter, id, onLetterClick) => {
-    const url = `/phrases/check`;
-    return fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        method: 'post',
-        body: JSON.stringify({
-            id,
-            letter,
+
+class Letter extends React.PureComponent {
+    state = {
+        isLetterCorrect: null,
+        usedLetter: false,
+    };
+
+    checkLetter = (letter, id) => {
+        const url = `/phrases/check`;
+        return fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            method: 'post',
+            body: JSON.stringify({
+                id,
+                letter,
+            })
         })
-    })
-    .then(response => response.json())
-    .catch(error => {
-        console.error(error);
-    })
-    .then(data => {
-        onLetterClick(data);
-    });
-}
+        .then(response => response.json())
+        .catch(error => {
+            console.error(error);
+        })
+        .then(data => {
+            if (data.failsCounter === this.props.failsCounter) {
+                this.setState({
+                    isLetterCorrect: true,
+                })
+            } else {
+                this.setState({
+                    isLetterCorrect: false,
+                })
+            }
+            this.props.onLetterClick(data);
+        });
+    }
 
-const Letter = React.memo(({letter, gameID, onLetterClick}) => {
-    return(
-        <div className={styles.letter} onClick={checkLetter.bind(null, letter, gameID, onLetterClick)}>
-            {letter}
-        </div>
-    )
-});
+    onClickMethod = () => {
+        if (this.state.usedLetter) return;
+        this.setState ({
+            usedLetter: true,
+        })
+        const { letter, gameID } = this.props;
+        this.checkLetter(letter, gameID);
+    }
+
+    render() {
+        const { usedLetter, isLetterCorrect } = this.state;
+
+        const letterStyle = classnames(styles.letter, {
+            [styles.letterHover]: !usedLetter,
+        }, usedLetter && isLetterCorrect !== null && {
+            [styles.correctLetter]: isLetterCorrect,
+            [styles.wrongLetter]: !isLetterCorrect,
+        });
+        return(
+            <div className={letterStyle} onClick={this.onClickMethod}>
+                {this.props.letter}
+            </div>
+        )
+    }
+}
 
 export default Letter;
--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -6,19 +6,23 @@ import { GAME_STATE_FAILED, GAME_STATE_WON } from '../../../consts';
 const GameOver = ({endState}) => {
     
     return(
-        <div className={styles.resultField} >
-            { endState === GAME_STATE_WON &&
-                <div className={classnames(styles.gameOverFrame, styles.win)}>
-                    <div>You guessed!</div>
-                    <div>Congratulations</div>
-                </div>
-            }
-            { endState === GAME_STATE_FAILED &&
-                <div className={classnames(styles.gameOverFrame, styles.loose)}>
-                    <div>You lost</div>
-                    <div>Try again</div>
-                </div>
-            }
+        <div>
+            <div className={styles.resultField}>
+                { endState === GAME_STATE_WON &&
+                    <div className={classnames(styles.gameOverFrame, styles.win)}>
+                        <div>You guessed!</div>
+                        <div>Congratulations</div>
+                    </div>
+                }
+                { endState === GAME_STATE_FAILED &&
+                    <div className={classnames(styles.gameOverFrame, styles.loose)}>
+                        <div>You lost</div>
+                        <div>Try again</div>
+                    </div>
+                }
+            </div>
+            <div className={styles.overlay}>
+            </div>
         </div>
     )
 }
