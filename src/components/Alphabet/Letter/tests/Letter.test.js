1683e947eeaefb3e41163795d0b072df8884154f
Dominik <dominik@pl.pl>
2018-11-13 22:25:01 +0100

    Set up babel & package.json configuration, added snapshots tests for Letter

--- src/components/Alphabet/Letter/tests/Letter.test.js
+++ src/components/Alphabet/Letter/tests/Letter.test.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Letter from '../Letter';
+import { LETTER_STATUS_CORRECT, LETTER_STATUS_INCORRECT } from '../../../../../consts';
 
 //1 method mount- mounts the whole tree of components
 //2 shallow- do shallow render
-test('Letter changes after click', () => {
-// Render a checkbox with label in the document
-    const component = shallow(
-        <Letter 
-            letter={letter}
-            gameID={gameID}
-            failsCounter={failsCounter}
-            onLetterClick={onLetterClick} 
-        />
-    );
-    console.log("debug", component.debug)
-    expect(true).toEqual(false);
-});
+// jest.fn() - creates anonymous function which always returns true
+
+describe('<Letter />', () => {
+    describe('Matches snapshot', () => {
+        it('letter is neither correct nor incorrect', () => {
+            const component = shallow(
+                <Letter 
+                    letter="D"
+                    gameID={0}
+                    onLetterClick={jest.fn()} 
+                />
+            );
+            expect(component).toMatchSnapshot();
+        });
+        
+        it('letter is correct', async () => {
+            const component = shallow(
+                <Letter 
+                    letter="D"
+                    gameID={0}
+                    onLetterClick={jest.fn()} 
+                />
+            );
+            await component.setState({ letterStatus: LETTER_STATUS_CORRECT });
+            expect(component).toMatchSnapshot();
+        });
+
+        it('letter is incorrect', async () => {
+            const component = shallow(
+                <Letter 
+                    letter="D"
+                    gameID={0}
+                    onLetterClick={jest.fn()} 
+                />
+            );
+            await component.setState({ letterStatus: LETTER_STATUS_INCORRECT });
+            expect(component).toMatchSnapshot();
+        });
+
+    })
+
+})
