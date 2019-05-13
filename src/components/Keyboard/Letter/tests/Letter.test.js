75d1bfa864c3f5b01b7dd18f5291ca372325f8d1
Dominik <dominik@pl.pl>
2019-05-13 21:13:38 +0200

    Merged with master better

--- src/components/Keyboard/Letter/tests/Letter.test.js
+++ src/components/Keyboard/Letter/tests/Letter.test.js
@@ -3,30 +3,30 @@ import { shallow } from 'enzyme';
 import Letter from '../Letter';
 import { LETTER_STATUS_CORRECT, LETTER_STATUS_INCORRECT } from '../../../../../consts';
 
-//1 method mount- mounts the whole tree of components
-//2 shallow- do shallow render
+// 1 method mount- mounts the whole tree of components
+// 2 shallow- do shallow render
 // jest.fn() - creates anonymous function which always returns true
 
 describe('<Letter />', () => {
     describe('Matches snapshot', () => {
         it('letter is neither correct nor incorrect', () => {
             const component = shallow(
-                <Letter 
+                <Letter
                     letter="D"
                     gameID={0}
-                    onLetterClick={jest.fn()} 
-                />
+                    onLetterClick={jest.fn()}
+                />,
             );
             expect(component).toMatchSnapshot();
         });
-        
+
         it('letter is correct', async () => {
             const component = shallow(
-                <Letter 
+                <Letter
                     letter="D"
                     gameID={0}
-                    onLetterClick={jest.fn()} 
-                />
+                    onLetterClick={jest.fn()}
+                />,
             );
             await component.setState({ letterStatus: LETTER_STATUS_CORRECT });
             expect(component).toMatchSnapshot();
@@ -34,11 +34,11 @@ describe('<Letter />', () => {
 
         it('letter is incorrect', async () => {
             const component = shallow(
-                <Letter 
+                <Letter
                     letter="D"
                     gameID={0}
-                    onLetterClick={jest.fn()} 
-                />
+                    onLetterClick={jest.fn()}
+                />,
             );
             await component.setState({ letterStatus: LETTER_STATUS_INCORRECT });
             expect(component).toMatchSnapshot();
@@ -47,29 +47,28 @@ describe('<Letter />', () => {
 
     it('clicking on letter calls API', () => {
         const onLetterClickMock = jest.fn();
-        
+
         const component = shallow(
-            <Letter 
+            <Letter
                 letter="A"
                 gameID={0}
-                onLetterClick={onLetterClickMock} 
-            />
-            );
+                onLetterClick={onLetterClickMock}
+            />,
+        );
 
         const instance = component.instance();
 
         const spy = jest.spyOn(instance, 'checkLetter');
         instance.forceUpdate();
-        
+
         const mockedFetchResult = {
             isLetterCorrect: true,
-        }
-        
+        };
+
         const mockedFetch = fetch.mockResponseOnce(JSON.stringify(mockedFetchResult));
 
         component.simulate('click');
         expect(mockedFetch).toHaveBeenCalled();
         expect(spy).toHaveBeenCalled();
-    })
-
+    });
 });
