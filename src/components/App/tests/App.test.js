548a4b9f44f8d86f10104691fcc8a8743312e601
Dominik <dominik@pl.pl>
2019-05-29 22:11:17 +0200

    Finding tests solutions..

--- src/components/App/tests/App.test.js
+++ src/components/App/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import toJson from 'enzyme-to-json';
+import { create } from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
 import { App } from '../App';
@@ -20,16 +20,55 @@ const initialState = {
 const mockStore = configureStore();
 let component;
 let store;
+let props;
 
 beforeEach(() => {
+    props = {
+        setGameState: jest.fn(),
+    };
     store = mockStore(initialState);
-    component = shallow(<App store={store} />);
+    component = shallow(<App {...props} />);
+    console.log('debug', component.debug());
 });
-
 
 describe('<App /> rendering', () => {
     it('renders the component', () => {
         expect(toJson(component)).toMatchSnapshot();
+    });
+});
+
+describe('<Add /> lifecycle method invocations', () => {
+    /* it('should change the state componentState componentDidMount method is invoked', () => {
+        expect(component.state('componentState')).toEqual('mounted');
+    }); */
+
+    /*
+    1#
+        const spy = jest.spyOn(Component.prototype, 'methodName');
+        const wrapper = mount(<Component {...props} />);
+        wrapper.instance().methodName();
+        expect(spy).toHaveBeenCalled();
+    2#
+        it("shows a list of users", async () => {
+            const component = create(<Users />);
+            const instance = component.getInstance();
+            await instance.componentDidMount();
+            console.log(instance.state) // << HERE IS THE SNITCH!
+        });
+    */
+    it('should call fetchPhrase during componentDidMount', async () => {
+        const fetchPhrase = jest.fn();
+        const instance = component.instance();
+        const spyCDM = jest.spyOn(instance, 'componentDidMount');
+        const fetchPhraseMock = jest.spyOn(instance, 'fetchPhrase');
+        await instance.componentDidMount();
+        expect(instance.componentDidMount).toHaveBeenCalledTimes(1);
+        expect(instance.fetchPhrase).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call setGameState during componentDidMount', () => {
+        const setGameStateFake = jest.spyOn(App.prototype, 'setGameState');
+        expect(setGameStateFake).toHaveBeenCalledTimes(1);
     });
 });
 
@@ -44,20 +83,4 @@ describe('<App /> rendering', () => {
         expect(component).toMatchSnapshot();
     });
 });
-
-describe('<Add /> lifecycle method invocations', () => {
-    it('should change the state componentState componentDidMount method is invoked', () => {
-        expect(component.state('componentState')).toEqual('mounted');
-    });
-
-    it('should call fetchPhrase during componentDidMount', () => {
-        const fetchPhraseFake = jest.spyOn(App.prototype, 'fetchPhrase');
-        expect(fetchPhraseFake).toHaveBeenCalledTimes(1);
-    });
-
-    it('should call setGameState during componentDidMount', () => {
-        const setGameStateFake = jest.spyOn(App.prototype, 'setGameState');
-        expect(setGameStateFake).toHaveBeenCalledTimes(1);
-    });
-});
- */
+*/
